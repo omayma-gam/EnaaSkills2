7634f1e33858b417a663a8d43cb91f58797ebdaa
omayma-gam <gamranioumayma@gmail.com>
2025-07-09 12:48:48 +0100

    La structure du projet

---
+++ Apprenant/src/test/java/org/example/apprenant/ApprenantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apprenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

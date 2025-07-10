a9fac0b544aed0e60969b0423242d33b4607c689
omayma-gam <gamranioumayma@gmail.com>
2025-07-10 10:28:15 +0100

    Modification du module

--- Brief-Service/src/main/java/ma/enaa/briefservice/Repository/BriefRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.enaa.briefservice.Repository;
-
-import ma.enaa.briefservice.Model.Brief;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BriefRepository extends JpaRepository<Brief,Long> {
-}

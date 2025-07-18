b00da02c24d6d0c5fb92d3bcf47ead3c33c09201
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-07-18 10:21:03 +0100

    AJOYE Eureka

---
+++ Eureka/src/test/java/ma/enaa/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enaa.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

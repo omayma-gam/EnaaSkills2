2060f6bbf9cbcccfb4e7e30e231159df23a80150
simodev744 <ammar.mohamed744@gmail.com>
2025-07-18 18:30:57 +0200

    editing code and adding api gatway

--- api-gateway/src/main/java/com/app/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/app/apigateway/ApiGatewayApplication.java
@@ -1,17 +1,15 @@
-package org.example.commpetence;
+package com.app.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-@EnableDiscoveryClient
+
 @SpringBootApplication
-@EnableFeignClients
-public class CommpetenceApplication {
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommpetenceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
-
